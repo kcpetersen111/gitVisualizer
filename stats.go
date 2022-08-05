5b8ba896c192fa8f908dc0e21f23309b419eecf2
Kaleb Petersen <kcpetersen111@gmail.com>
2022-08-04 20:33:26 -0600

    there is a weird package issue ill solve when I have internet

--- stats.go
+++ stats.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"sort"
 	"time"
 
 	"github.com/go-git/go-git/plumbing/object"
@@ -8,8 +10,13 @@ import (
 )
 
 const (
-	daysInLastSixMonths = 183
-	outOfRange          = 99999
+	daysInLastSixMonths  = 183
+	outOfRange           = 99999
+	weeksInLastSixMonths = 26
+)
+
+type (
+	column []int
 )
 
 //given an email will return the commits made in the last 6 months
@@ -47,6 +54,7 @@ func fillCommits(email, path string, commits map[int]int) map[int]int {
 	}
 	//iterate the commits
 	offset := calcOffset()
+	var c object.Commit
 	err = iterator.ForEach(func(c *object.Commit) error {
 		daysAgo := countDaysSinceDate(c.Author.When) + offset
 		if c.Author.Email != email {
@@ -103,6 +111,132 @@ func calcOffset() int {
 
 	}
 	return offset
+}
+
+func printCommitsStats(commits map[int]int) {
+	keys := sortMapIntoSlice(commits)
+	cols := buildCols(keys, commits)
+	printCells(cols)
+}
+
+func sortMapIntoSlice(m map[int]int) []int {
+	var keys []int
+	for k := range m {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+	return keys
+}
+
+func buildCols(keys []int, commits map[int]int) map[int]column {
+	cols := make(map[int]column)
+	col := column{}
+
+	for _, k := range keys {
+		week := int(k / 7)
+		dayinweek := k % 7
+
+		if dayinweek == 0 {
+			col = column{}
+		}
+		col = append(col, commits[k])
+
+		if dayinweek == 6 {
+			cols[week] = col
+		}
+	}
+	return cols
+}
+
+// will print out all of the cells of the graph
+func printCells(cols map[int]column) {
+	printMonths()
+	for j := 6; j >= 0; j-- {
+		for i := weeksInLastSixMonths + 1; i >= 0; i-- {
+			if i == weeksInLastSixMonths+1 {
+				printDayCol(j)
+			}
+			if col, ok := cols[i]; ok {
+				//the special case of today
+				if i == 0 && j == calcOffset()-1 {
+					printCell(col[j], true)
+					continue
+				} else {
+					if len(col) > j {
+						printCell(col[j], false)
+						continue
+					}
+				}
+			}
+			printCell(0, false)
+		}
+		fmt.Printf("\n")
+	}
+}
+
+// will print the month
+func printMonths() {
+	week := getBeginningOfDay(time.Now()).Add(-(daysInLastSixMonths * time.Hour * 24))
+	month := week.Month()
+	fmt.Printf("		")
+	for {
+		if week.Month() != month {
+			fmt.Printf("%s ", week.Month().String()[:3])
+			month = week.Month()
+		} else {
+			fmt.Printf("		")
+		}
+
+		week = week.Add(7 * time.Hour * 24)
+		if week.After(time.Now()) {
+			break
+		}
+	}
+	fmt.Printf("\n")
+}
+
+func printDayCol(day int) {
+	out := "		"
+	switch day {
+	case 1:
+		out = " Mon "
+	case 3:
+		out = " Wed "
+	case 5:
+		out = " Fri "
+	}
+	fmt.Printf(out)
+}
+
+func printCell(val int, today bool) {
+	//tbh idk what these strings mean but i will test them later to learn if I remember to
+	escape := "\033[0;37;30m"
+	switch {
+	case val > 0 && val < 5:
+		escape = "\033[1;30;47m"
+	case val >= 5 && val < 10:
+		escape = "\033[1;30;43m"
+	case val >= 10:
+		escape = "\033[1;30;42m"
+	}
+
+	if today {
+		escape = "\033[1;37;45m"
+	}
+
+	if val == 0 {
+		fmt.Printf(escape + " - " + "\033[0m")
+		return
+	}
+	str := "  %d "
+	switch {
+	case val >= 10:
+		str = " %d "
+	case val >= 100:
+		str = "%d "
+	}
+
+	fmt.Printf(escape+str+"\033[0m", val)
 }
 
 //generate a nice graph
